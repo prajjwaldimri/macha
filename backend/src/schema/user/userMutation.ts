4e8d614ff99b49b089eb94dfcc539f9a8399b457
Subarna Dhar <subarnadhar22@gmail.com>
2020-01-27 15:56:15 +0530

    Added signup query with username and password(without hashed)

--- backend/src/schema/user/userMutation.ts
+++ backend/src/schema/user/userMutation.ts
@@ -1,6 +1,18 @@
 import { stringArg, mutationField } from "nexus";
-
+import { UserModel } from "../../models/User";
+import { User } from "./user";
 export const loginUser = mutationField("login", {
   type: "User",
   args: {}
+});
+
+export const signUpUser = mutationField("signup", {
+  type: "User",
+  args: {
+    username: stringArg({ required: true }),
+    password: stringArg({ required: true })
+  },
+  async resolve(_, { username, password }) {
+    return await UserModel.create({ username, password });
+  }
 });
