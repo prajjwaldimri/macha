ab5df3a11e6e677a6de1cab2b3c380d15aece346
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-01-27 15:57:23 +0530

    Added args to loginUser

--- backend/src/schema/user/userMutation.ts
+++ backend/src/schema/user/userMutation.ts
@@ -2,5 +2,9 @@ import { stringArg, mutationField } from "nexus";
 
 export const loginUser = mutationField("login", {
   type: "User",
-  args: {}
+  args: {
+    username: stringArg({ required: true }),
+    password: stringArg({ required: true })
+  },
+  async resolve(_, { username, password }) {}
 });
