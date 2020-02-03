f134a8f027a422a13fb714371000685ba9fff9fa
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-02-03 15:52:31 +0530

    removed unnecessary mongoose model creations from app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,8 +17,6 @@ import * as jwt from "jsonwebtoken";
 
 import * as allTypes from "./schema";
 import { UserModel } from "./models/User";
-import { CommentModel } from "./models/Comment";
-import { TextPostModel } from "./models/TextPost";
 
 const schema = makeSchema({
   types: allTypes,
@@ -52,19 +50,6 @@ mongoose
   .then(() => {
     server.listen().then(async ({ url }) => {
       debug(`🚀 Server ready at ${url}`);
-      const post = await TextPostModel.create({
-        author: "5e354de85354367aaeb76ded",
-        uri: "yoyoy",
-        content: "IOAUDIASYDIYASIs"
-      });
-      CommentModel.create({
-        author: "5e354de85354367aaeb76ded",
-        text: "Hello",
-        postType: "VideoPost",
-        post: post._id
-      }).then(doc => {
-        console.log(doc);
-      });
     });
   })
   .catch(err => {
