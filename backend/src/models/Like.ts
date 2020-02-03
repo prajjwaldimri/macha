d7797194120acc68fb5c1fd0c2f646598f3d9b38
Subarna Dhar <subarnadhar22@gmail.com>
2020-02-03 15:55:16 +0530

    Added enum and postType to Like.ts #23hd8b

--- backend/src/models/Like.ts
+++ backend/src/models/Like.ts
@@ -1,5 +1,14 @@
 import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
 import { User } from "./User";
+import { ImagePost } from "./ImagePost";
+import { VideoPost } from "./VideoPost";
+import { TextPost } from "./TextPost";
+
+enum PostType {
+  ImagePost = "ImagePost",
+  VideoPost = "VideoPost",
+  TextPost = "TextPost"
+}
 
 class Like {
   @prop({
@@ -7,6 +16,18 @@ class Like {
     ref: "User"
   })
   author!: Ref<User>;
+
+  @prop({
+    required: true,
+    enum: PostType
+  })
+  postType!: PostType;
+
+  @prop({
+    required: true,
+    refPath: "postType"
+  })
+  post!: Ref<ImagePost | TextPost | VideoPost>;
 }
 
 export const LikeModel = getModelForClass(Like);
