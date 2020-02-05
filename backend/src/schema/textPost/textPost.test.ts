190dfec14b9ca2483c43d9214550bd29abfcad93
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-02-05 15:04:33 +0530

    Added updateTextPost mutation and its tests #23jz5g

--- backend/src/schema/textPost/textPost.test.ts
+++ backend/src/schema/textPost/textPost.test.ts
@@ -15,6 +15,7 @@ const server = new ApolloServer({ schema });
 const { query, mutate } = createTestClient(server);
 
 let authorizedApolloClient: ApolloServerTestClient;
+let authorizedApolloClient2: ApolloServerTestClient;
 
 import { before, after } from "../testutils";
 import { UserModel } from "../../models/User";
@@ -36,7 +37,20 @@ test.before(async () => {
     }
   });
 
+  await mutate({
+    mutation: CREATEUSER,
+    variables: {
+      username: "test@#!use*(--2",
+      password: ".sdasdad*&^^%$Jmandb   sdas",
+      name: "Test User 2"
+    }
+  });
+
   const user = await UserModel.findOne({ username: "test@#!use*(" }).select(
+    "-password"
+  );
+
+  const user2 = await UserModel.findOne({ username: "test@#!use*(--2" }).select(
     "-password"
   );
 
@@ -46,6 +60,17 @@ test.before(async () => {
       context: () => {
         return {
           user: user
+        };
+      }
+    })
+  );
+
+  authorizedApolloClient2 = createTestClient(
+    new ApolloServer({
+      schema,
+      context: () => {
+        return {
+          user: user2
         };
       }
     })
@@ -78,6 +103,7 @@ test.serial("should create text post", async t => {
   t.assert(!result.errors);
   t.assert(result.data);
   t.assert(result.data!.createTextPost.content === "Test Content");
+  t.assert(post!.content === "Test Content");
 });
 
 test.serial("should not create text post (same-uri)", async t => {
@@ -109,6 +135,75 @@ test.serial("should not create text post (no content)", async t => {
 test("shouldn't create text post (not logged in)", async t => {
   const result = await mutate({
     mutation: CREATETEXTPOST,
+    variables: {
+      uri: "test-uri",
+      content: "Test Content"
+    }
+  });
+
+  t.assert(result.errors);
+  t.assert(!result.data);
+});
+
+//#endregion
+
+//#region Update Text Post
+
+const UPDATETEXTPOST = gql`
+  mutation updateTextPost($uri: String!, $content: String!) {
+    updateTextPost(uri: $uri, content: $content) {
+      content
+    }
+  }
+`;
+
+test.serial("should update text post", async t => {
+  const result = await authorizedApolloClient.mutate({
+    mutation: UPDATETEXTPOST,
+    variables: {
+      uri: "test-uri",
+      content: "Test Content 23"
+    }
+  });
+
+  const post = await TextPostModel.findOne({ uri: "test-uri" });
+
+  t.assert(post);
+  t.assert(!result.errors);
+  t.assert(result.data);
+  t.assert(result.data!.updateTextPost.content === "Test Content 23");
+  t.assert(post!.content === "Test Content 23");
+});
+
+test.serial("should not update text post (wrong-logged-in-user)", async t => {
+  const result = await authorizedApolloClient2.mutate({
+    mutation: UPDATETEXTPOST,
+    variables: {
+      uri: "test-uri",
+      content: "Test Content"
+    }
+  });
+
+  t.assert(result.errors);
+  t.assert(!result.data);
+});
+
+test.serial("should not update text post (no content)", async t => {
+  const result = await authorizedApolloClient.mutate({
+    mutation: UPDATETEXTPOST,
+    variables: {
+      uri: "test-uri",
+      content: ""
+    }
+  });
+
+  t.assert(result.errors);
+  t.assert(!result.data);
+});
+
+test("shouldn't update text post (not logged in)", async t => {
+  const result = await mutate({
+    mutation: UPDATETEXTPOST,
     variables: {
       uri: "test-uri",
       content: "Test Content"
--- backend/src/schema/textPost/textPostMutation.ts
+++ backend/src/schema/textPost/textPostMutation.ts
@@ -1,4 +1,8 @@
-import { UserInputError, AuthenticationError } from "apollo-server";
+import {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError
+} from "apollo-server";
 import { stringArg, mutationField, intArg } from "nexus";
 import { TextPostModel } from "../../models/TextPost";
 import isLength from "validator/lib/isLength";
@@ -30,6 +34,49 @@ export const createTextPost = mutationField("createTextPost", {
         uri,
         content
       });
+    } catch (err) {
+      return err;
+    }
+  }
+});
+
+export const updateTextPost = mutationField("updateTextPost", {
+  type: "TextPost",
+  args: {
+    uri: stringArg({ required: true }),
+    content: stringArg({ required: true })
+  },
+  async resolve(_, { uri, content }, ctx): Promise<any> {
+    try {
+      if (!ctx.user) {
+        throw new AuthenticationError(
+          "Cannot update a post without logging in"
+        );
+      }
+
+      const textPost = await TextPostModel.findOne({ uri });
+
+      if (!textPost) {
+        throw new UserInputError("uri doesn't exist");
+      }
+
+      if (textPost.author.toString() !== ctx.user._id.toString()) {
+        throw new ForbiddenError(
+          "Not allowed to update the post as the logged in user is not the author of the post"
+        );
+      }
+
+      if (!isLength(content.trim(), { min: 1 })) {
+        throw new UserInputError("content should not be empty");
+      }
+
+      return await TextPostModel.findOneAndUpdate(
+        { uri },
+        {
+          content
+        },
+        { new: true }
+      );
     } catch (err) {
       return err;
     }
