14542663e836201e035fe76fc5a6a61a8ca56754
Subarna Dhar <subarnadhar22@gmail.com>
2020-02-09 15:53:54 +0530

    Added deleteVideoPost resolver #23jz5j

--- backend/src/schema/videoPost/videoPostMutation.ts
+++ backend/src/schema/videoPost/videoPostMutation.ts
@@ -7,7 +7,6 @@ import { stringArg, mutationField, intArg } from "nexus";
 import { VideoPostModel } from "../../models/VideoPost";
 import isLatLong from "validator/lib/isLatLong";
 import { UserContext } from "../types";
-import { resolve } from "dns";
 
 export const createVideoPost = mutationField("createVideoPost", {
   type: "VideoPost",
@@ -57,32 +56,68 @@ export const updateVideoPost = mutationField("updateVideoPost", {
     location: stringArg(),
     caption: stringArg()
   },
-  async resolve(_, { uri, location, caption }, ctx: UserContext): Promise<any>{
-    try{
-      if(!ctx.user){
+  async resolve(_, { uri, location, caption }, ctx: UserContext): Promise<any> {
+    try {
+      if (!ctx.user) {
         throw new AuthenticationError(
           "Cannot update a post without logging in"
         );
       }
 
-      const videoPost = await VideoPostModel.findOne({uri});
+      const videoPost = await VideoPostModel.findOne({ uri });
 
-      if(!videoPost){
+      if (!videoPost) {
         throw new UserInputError("uri doesn't exist");
       }
 
-      if(videoPost.author.toString()!== ctx.user._id.toString()){
-        throw new ForbiddenError("Not allowed to update the post as the logged in user is not the author of the post");
+      if (videoPost.author.toString() !== ctx.user._id.toString()) {
+        throw new ForbiddenError(
+          "Not allowed to update the post as the logged in user is not the author of the post"
+        );
       }
 
       return await VideoPostModel.findOneAndUpdate(
-        {uri},
+        { uri },
         {
           location,
           caption
         },
-        {new: true}
+        { new: true }
       );
+    } catch (err) {
+      return err;
+    }
+  }
+});
+
+export const deleteVideoPost = mutationField("deleteVideoPost", {
+  type: "VideoPost",
+  args: {
+    uri: stringArg({ required: true })
+  },
+  async resolve(_, { uri }, ctx: UserContext): Promise<any> {
+    try {
+      if (!ctx.user) {
+        throw new AuthenticationError(
+          "Cannot delete a post without logging in"
+        );
+      }
+
+      const videoPost = await VideoPostModel.findOne({ uri });
+
+      if (!videoPost) {
+        throw new UserInputError("uri doesn't exist");
+      }
+
+      if (videoPost.author.toString() !== ctx.user._id.toString()) {
+        throw new ForbiddenError(
+          "Not allowed to delete the post as the logged in user is notthe author of the post"
+        );
+      }
+
+      return await videoPost.remove();
+    } catch (err) {
+      return err;
     }
   }
 });
