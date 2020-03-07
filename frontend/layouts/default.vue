bd68d52c6b6704f4961d9dc44d42a76615d8ce36
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-03-07 19:25:43 +0530

    Added automatic detection of dark theme

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,5 +4,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      this.$vuetify.theme.dark = true;
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(function(e) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        this.$vuetify.theme.dark = true;
+      } else {
+        this.$vuetify.theme.dark = false;
+      }
+    });
+  }
+};
 </script>
