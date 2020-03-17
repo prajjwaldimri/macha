ce6194b2a431a7c73668d3e11f8725f03a6449e3
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-03-17 15:45:34 +0530

    Added link sharing capability

--- frontend/pages/profile/index.vue
+++ frontend/pages/profile/index.vue
@@ -1,22 +1,56 @@
 <template lang="pug">
-  v-toolbar(prominent flat height="240")
-    .top-profile.pt-5
-      v-avatar(color="primary" size="80")
-        span.white--text.headline PD
-      span.title.pt-2 {{user.name}}
+  .profile
+    v-toolbar(prominent flat height="240")
+      .top-profile.pt-5
+        v-avatar(color="primary" size="80")
+          span.white--text.headline PD
+        span.title.pt-2 {{user.name}}
 
-    template(v-slot:extension)
-      v-tabs(v-model="tab" grow show-arrows)
-        v-tab(key="profile") Profile
-        v-tab(key="friends") Friends
-        v-tab(key="photos") Photos
-        v-tab(key="settings") Settings
+      template(v-slot:extension)
+        v-tabs(v-model="tab" grow show-arrows)
+          v-tab(key="profile") Profile
+          v-tab(key="friends") Friends
+          v-tab(key="photos") Photos
+          v-tab(key="settings") Settings
+
+    v-fab-transition
+      v-btn(color="primary" fixed fab large bottom right @click.stop="dialog=true")
+        v-icon mdi-account-multiple-plus
+
+    v-dialog(v-model="dialog" width="85%" height="85%")
+      v-card
+        v-card-title.headline Add a Macha
+        v-card-text You can add a macha by sharing them your link or asking them to scan your QR Code.
+
+        v-card-text Send this link to your friend
+        v-row(justify="center")
+          v-btn(@click="share" outlined small)
+            v-icon(small).mr-2 mdi-share
+            | Share your link
+        h3(style="text-align: center").heading.mt-3.mb-2 OR
+        v-card-text From your friend's device scan this QR Code
+
+        v-card-actions
+          v-btn(@click="dialog = false" small)
+            v-icon(small).mr-2 mdi-refresh
+            | Generate new link
+          v-spacer
+          v-btn(@click="dialog = false" outlined color="primary" small) Done
+
 </template>
 
 <script>
 import profile from '~/gql/profile';
+import qrcode from 'qrcode';
 
 export default {
+  data() {
+    return {
+      tab: 'profile-tab',
+      user: {},
+      dialog: false
+    };
+  },
   async mounted() {
     try {
       const token = this.$apolloHelpers.getToken();
@@ -29,18 +63,27 @@ export default {
         })
         .then(({ data }) => data.me);
     } catch (e) {
-      console.log(e);
-      //TODO: Redirect to login page
+      await this.$apolloHelpers.onLogout();
+      this.$router.replace('/login');
       this.$notifier.showErrorMessage({
         content: 'You need to be logged in to view the profile page'
       });
     }
   },
-  data() {
-    return {
-      tab: 'profile-tab',
-      user: {}
-    };
+  methods: {
+    async share() {
+      if (navigator.share) {
+        await navigator.share({
+          text: 'Hey, be my friend @ macha.in by clicking on this link',
+          url: `https://macha.in/addFriend/${this.user.uniqueMachaId}`
+        });
+      } else {
+        this.$sharer.showSheet({
+          text: 'Hey, be my friend @ macha.in by clicking on this link',
+          url: `https://macha.in/addFriend/${this.user.uniqueMachaId}`
+        });
+      }
+    }
   }
 };
 </script>
@@ -54,5 +97,9 @@ export default {
   justify-content: center;
   align-items: center;
   letter-spacing: 0.1rem;
+}
+
+.profile {
+  height: 100vh;
 }
 </style>
