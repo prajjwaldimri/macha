70f0432e72183407a41c4de44e56b0d691152d53
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-03-17 20:09:19 +0530

    Added profile pics from adorable avatars

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -84,6 +84,7 @@ export default {
       }
     }
   },
+
   /*
    ** Build configuration
    */
---
+++ frontend/pages/profile/friends.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+v-list(flat two-line)
+  v-list-item(v-for="macha in machas" :key="macha.username" @click="")
+    v-list-item-avatar
+      v-avatar
+        img(src="https://cdn.vuetifyjs.com/images/john.jpg")
+
+    v-list-item-content
+      v-list-item-title(v-text="macha.name")
+      v-list-item-subtitle(v-text="macha.username")
+
+    v-list-item-action
+      v-btn(icon)
+        v-icon(color="error") mdi-account-multiple-remove
+</template>
+
+<script>
+import getMachas from '~/gql/getMachas';
+
+export default {
+  data() {
+    return {
+      machas: [{ name: 'Hello', username: 'asiusaui' }]
+    };
+  },
+  async mounted() {}
+};
+</script>
