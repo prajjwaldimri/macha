49f1def6e9ba94a20176043771487f4e275a504c
Subarna Dhar <subarnadhar22@gmail.com>
2020-04-07 13:07:03 +0530

    Added nanoid for uri in textpost and videopost model

--- backend/src/models/TextPost.ts
+++ backend/src/models/TextPost.ts
@@ -1,22 +1,24 @@
 import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
 import { User } from "./User";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import nanoid from "nanoid";
 
 export class TextPost extends TimeStamps {
   @prop({
     required: true,
-    ref: "User"
+    ref: "User",
   })
   author!: Ref<User>;
 
   @prop({
     required: true,
-    unique: true
+    unique: true,
+    default: () => nanoid(),
   })
   uri!: string;
 
   @prop({
-    required: true
+    required: true,
   })
   content!: string;
 }
