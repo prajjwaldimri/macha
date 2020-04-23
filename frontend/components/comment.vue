936640ada1964acbdca51ee6c7b1a9e47e7baa90
Subarna Dhar <subarnadhar22@gmail.com>
2020-04-23 16:58:16 +0530

    Added like and like count feature to comments

--- frontend/components/comment.vue
+++ frontend/components/comment.vue
@@ -1,14 +1,20 @@
 <template lang="pug">
   v-card(:loading="isCommentsLoading" flat)
     v-list-item(v-for='comment in comments' :key='comment.id' dense three-line)
-      v-list-item-avatar(v-if ="comment.authorDetails.profileImage" size)
+      v-list-item-avatar(v-if ="comment.authorDetails.profileImage" size).px-0
         v-img(:src="comment.authorDetails.profileImage" aspect-ratio="1")
       v-list-item-content
         v-list-item-title {{comment.authorDetails.name}} 
         v-list-item-subtitle {{comment.text}}
       v-list-item-action
+        v-btn(v-if="comment.hasCurrentUserLikedComment" icon @click="toggleLikeComment(comment)" color="pink" left :disabled="isCommentsLoading" :loading="isLikeLoading")
+          v-icon(small) mdi-heart
+          span.pl-0  {{comment.likeCount}}
+        v-btn(icon @click="toggleLikeComment(comment)" color="pink" :disabled="isCommentsLoading" :loading="isLikeLoading" v-else)
+          v-icon(small) mdi-heart-outline
+          span.pl-0  {{comment.likeCount}}
         v-btn(icon v-if="comment.isCurrentUserAuthor" @click="deleteComment(comment.id)" color="error" :disabled="isCommentsLoading")
-          v-icon(small) mdi-delete
+          v-icon(small).pr-3 mdi-delete
 
 </template>
 
@@ -16,6 +22,9 @@
 <script>
 import getCommentsForThePost from '../gql/getCommentsForThePost';
 import deleteComment from '../gql/deleteComment';
+import isCurrentUserLiker from '../gql/isCurrentUserLiker';
+import likeComment from '../gql/likeComment';
+import unlikeComment from '../gql/unlikeComment';
 
 export default {
   props: {
@@ -30,7 +39,8 @@ export default {
       comments: [],
       skip: 0,
       limit: 15,
-      isCommentsLoading: false
+      isCommentsLoading: false,
+      isLikeLoading: false
     };
   },
   methods: {
@@ -71,12 +81,49 @@ export default {
         this.refresh();
         this.$emit('postDeleted');
       } catch (e) {
-        console.log(e);
         this.$notifier.showErrorMessage({
           content: 'Error deleting your comment'
         });
       } finally {
         this.isCommentsLoading = false;
+      }
+    },
+    async toggleLikeComment(comment) {
+      try {
+        this.isLikeLoading = true;
+        if (comment.hasCurrentUserLikedComment) {
+          await this.$apollo.mutate({
+            mutation: unlikeComment,
+            variables: {
+              commentId: comment.id
+            }
+          });
+          comment.likeCount -= 1;
+        } else {
+          await this.$apollo.mutate({
+            mutation: likeComment,
+            variables: {
+              commentId: comment.id
+            }
+          });
+          comment.likeCount += 1;
+        }
+        comment.hasCurrentUserLikedComment = await this.$apollo
+          .query({
+            query: isCurrentUserLiker,
+            variables: {
+              identifier: comment.id
+            },
+            fetchPolicy: 'network-only'
+          })
+          .then(({ data }) => data.isCurrentUserLiker);
+      } catch (e) {
+        console.log(e);
+        this.$notifier.showErrorMessage({
+          content: 'Error chaging the status of like on the comment.'
+        });
+      } finally {
+        this.isLikeLoading = false;
       }
     }
   }
