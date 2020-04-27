5ecb932feaf39907577e0b32da19d2ae4f483029
Subarna Dhar <subarnadhar22@gmail.com>
2020-04-27 19:05:59 +0530

    Made 404 page as a separate component

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,14 +1,7 @@
 <template lang="pug">
   .errorPage
-    #notFound(v-if='error.statusCode === 404')
-      div.makeCenter
-        span 404
-        p Seems like you are lost. 
-        p Don't worry! We got you covered.
-        NuxtLink(to='/').homeButton
-          v-btn(raised color="primary") 
-            .homeText
-              | GO HOME
+    div(v-if='error.statusCode === 404')
+      notFound
     h1(v-else='')
       | {{ otherError }}
       NuxtLink(to='/')
@@ -16,8 +9,13 @@
 </template>
 
 <script>
+import notFound from '../components/error/notFound';
+
 export default {
   layout: 'empty',
+  components: {
+    notFound
+  },
   props: {
     error: {
       type: Object,
@@ -26,7 +24,6 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
       otherError: 'An error occurred'
     };
   },
@@ -39,62 +36,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
-#notFound span {
-  display: flex;
-  font-size: 100pt;
-  font-weight: bolder;
-  justify-content: center;
-  color: #ffffff;
-}
-#notFound p {
-  display: flex;
-  font-size: 10pt;
-  font-weight: bolder;
-  justify-content: center;
-  color: #ffffff;
-  line-height: 0pt;
-}
-#notFound .homeButton {
-  display: flex;
-  justify-content: center;
-  text-decoration: none;
-  padding-top: 12px;
-}
-#notFound .homeButton .homeText {
-  color: black;
-  text-decoration: none;
-}
-#notFound {
-  position: relative;
-  background-image: url('~assets/art.jpg');
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  opacity: 90%;
-  font-family: 'Special Elite', cursive;
-}
-#notFound::before {
-  content: '';
-  position: absolute;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  background-color: rgba(0, 0, 0, 0.3);
-}
-.makeCenter {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  width: auto;
-  height: auto;
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-}
-</style>
