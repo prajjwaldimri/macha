59088443f32eb5f53c3f351dd7c37a2f48f18307
Subarna Dhar <subarnadhar22@gmail.com>
2020-04-27 18:36:10 +0530

    Completed the basic design of 404 page

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
   .errorPage
-    .notFound( v-if='error.statusCode === 404' )
-      //-v-img(:src="require()" aspect-ratio="1")
-      | {{ pageNotFound }}
+    #notFound(v-if='error.statusCode === 404')
+      div.makeCenter
+        span 404
+        p Seems like you are lost. 
+        p Don't worry! We got you covered.
+        NuxtLink(to='/').homeButton
+          v-btn(raised color="primary") 
+            .homeText
+              | GO HOME
     h1(v-else='')
       | {{ otherError }}
-    NuxtLink(to='/')
-      | Home page
+      NuxtLink(to='/')
+        | Home page
 </template>
 
 <script>
@@ -35,12 +41,50 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+#notFound span {
+  display: flex;
+  font-size: 100pt;
+  font-weight: bolder;
+  justify-content: center;
+  color: #ffffff;
 }
-.notFound {
-  background-image: url('~assets/404image.jpg');
-  height: 95vh;
+#notFound p {
+  display: flex;
+  font-size: 10pt;
+  font-weight: bolder;
+  justify-content: center;
+  color: #ffffff;
+  line-height: 0pt;
+}
+#notFound .homeButton {
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+  padding-top: 12px;
+}
+#notFound .homeButton .homeText {
+  color: black;
+  text-decoration: none;
+}
+#notFound {
+  background-image: url('~assets/art.jpg');
+  height: 100vh;
   background-size: cover;
+  background-position: center;
+  opacity: 90%;
+  font-family: 'Special Elite', cursive;
+}
+.makeCenter {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: auto;
+  height: auto;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 }
 </style>
