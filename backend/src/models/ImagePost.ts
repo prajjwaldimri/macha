bd6aba99d3c9d24d76763f55b401dd7028e1a676
Subarna Dhar <subarnadhar22@gmail.com>
2020-04-30 21:00:08 +0530

    Added notifications to image post

--- backend/src/models/ImagePost.ts
+++ backend/src/models/ImagePost.ts
@@ -1,19 +1,20 @@
 import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
-import { User } from "./User";
+import { User, UserModel } from "./User";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import nanoid from "nanoid";
+import { NotificationModel } from "./Notification";
 
 export class ImagePost extends TimeStamps {
   @prop({
     required: true,
-    ref: "User"
+    ref: "User",
   })
   author!: Ref<User>;
 
   @prop({
     required: true,
     unique: true,
-    default: () => nanoid()
+    default: () => nanoid(),
   })
   uri!: string;
 
@@ -28,3 +29,48 @@ export class ImagePost extends TimeStamps {
 }
 
 export const ImagePostModel = getModelForClass(ImagePost);
+
+ImagePostModel.watch().on("change", async (data: any) => {
+  if (data.fullDocument) {
+    //Insert operation
+    const user = await UserModel.findById(
+      data.fullDocument.author.toString()
+    ).populate({
+      path: "machas",
+      select: "id",
+    });
+    let machas = user!.machas?.flatMap((macha) => (macha as any)._id);
+    if (!machas) {
+      return;
+    }
+    for (let macha of machas) {
+      let notification = {
+        content: `${user!.name} created a new post.`,
+        user: macha,
+        uri: `https://macha.in/image/${data.fullDocument.uri}`,
+      };
+
+      await NotificationModel.create(notification);
+    }
+  } else if (data.updateDescription) {
+    // Update operation
+    const post = await ImagePostModel.findById(data.documentKey._id);
+    const user = await UserModel.findById(post!.author.toString()).populate({
+      path: "machas",
+      select: "_id",
+    });
+    let machas = user!.machas?.flatMap((macha) => (macha as any)._id);
+    if (!machas) {
+      return;
+    }
+    for (let macha of machas) {
+      let notification = {
+        content: `${user!.name} updated their post.`,
+        user: macha,
+        uri: `https://macha.in/image/${post!.uri}`,
+      };
+
+      await NotificationModel.create(notification);
+    }
+  }
+});
