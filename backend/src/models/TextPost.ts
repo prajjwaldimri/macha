1753268dcd19065f15a9242aae3118ece90a39fc
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-04-30 18:29:03 +0530

    Added notification system for text post creation

--- backend/src/models/TextPost.ts
+++ backend/src/models/TextPost.ts
@@ -1,7 +1,8 @@
 import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
-import { User } from "./User";
+import { User, UserModel } from "./User";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import nanoid from "nanoid";
+import { NotificationModel } from "./Notification";
 
 export class TextPost extends TimeStamps {
   @prop({
@@ -24,3 +25,31 @@ export class TextPost extends TimeStamps {
 }
 
 export const TextPostModel = getModelForClass(TextPost);
+
+TextPostModel.watch().on("change", async (data: any) => {
+  // https://mongoosejs.com/docs/models.html#change-streams
+  if (data.fullDocument) {
+    // Insert operation
+    const user = await UserModel.findById(
+      data.fullDocument.author.toString()
+    ).populate({
+      path: "machas",
+      select: "_id",
+    });
+    let machas = user!.machas?.flatMap((macha) => (macha as any)._id);
+    if (!machas) {
+      return;
+    }
+    for (let macha of machas) {
+      let notification = {
+        content: `${user!.name} created a new post \"${
+          data.fullDocument.content
+        }\"`,
+        user: macha,
+        uri: `https://macha.in/text/${data.fullDocument.uri}`,
+      };
+
+      await NotificationModel.create(notification);
+    }
+  }
+});
