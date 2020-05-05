2961a6c46791cc6e9bd405e96f3b022caa48a87d
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2020-05-05 15:28:25 +0530

    Fixed error messages on login

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -115,7 +115,7 @@ export default {
       } catch (e) {
         this.$store.dispatch('error/addError', e);
         this.$notifier.showErrorMessage({
-          content: e.graphQLErrors[0].message
+          content: e
         });
       }
     },
@@ -149,7 +149,7 @@ export default {
       } catch (e) {
         this.$store.dispatch('error/addError', e);
         this.$notifier.showErrorMessage({
-          content: e.graphQLErrors[0].message
+          content: e
         });
       }
     }
